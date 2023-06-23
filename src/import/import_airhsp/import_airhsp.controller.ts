f621dae0e39591e88c9b99409b4a83c63b85d778
JesusMayuri7 <jesus.mayuri.cuellar@gmail.com>
2023-06-22 21:39:23 -0500

    Version imported file y access workbook

--- src/import/import_airhsp/import_airhsp.controller.ts
+++ src/import/import_airhsp/import_airhsp.controller.ts
@@ -1,13 +1,36 @@
-import {Controller,Post,UseInterceptors,UploadedFile,UploadedFiles} from '@nestjs/common';
+import {Controller,Post,UseInterceptors,UploadedFile,UploadedFiles, ParseFilePipe, MaxFileSizeValidator, FileTypeValidator} from '@nestjs/common';
 import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
+import * as Excel from 'exceljs';
+import {createWriteStream} from 'fs'
 
   @Controller('import-airhsp')
   export class ImportAirhspController {
 
     @Post('upload')
-    @UseInterceptors(FileInterceptor('file', { dest: './uploads' }))
-    uploadSingle(@UploadedFile() file) {
-      console.log(file);
+    @UseInterceptors(FileInterceptor('file'))
+    async uploadSingle(@UploadedFile(new ParseFilePipe({
+      validators: [
+        new MaxFileSizeValidator({ maxSize: 5 * 1024 * 1024 }),
+        new FileTypeValidator({ fileType: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' }),
+      ],
+    })) file:Express.Multer.File) {
+       console.log('ok');
+      const workbook = new Excel.Workbook();
+      await workbook.xlsx.load(file.buffer);
+      var worksheet = workbook.worksheets[0];
+      worksheet.eachRow((row,rowNumber)=>{
+        if(rowNumber == 4) {
+          row.eachCell((celL) => {
+            console.log(celL.text);
+          });
+        }
+      });
+
+      const ws = createWriteStream('./uploads/'+file.originalname);
+      ws.write(file.buffer)
+      //console.log(workbook.getWorksheet[0]);     
+    
+ 
     }
     
     @Post('uploads')
